afe393092e05df05e9b51a6b4b86284dc28e4ecd
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-04 16:07:54 -0400

    Currently working need to rework for prefabs

---
+++ AutomaticDoor/Assets/Scripts/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door;
+    public float closeSpeed;
+    bool open = false;
+    Vector3 closing;
+    Vector3 closedPosition;
+    Vector3 openPosition;
+
+    private void Start()
+    {
+        closing.y = -1.0f;
+        closedPosition = door.transform.position;
+        openPosition.y = closedPosition.y + door.transform.lossyScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open == true)
+        {
+            door.transform.Translate(openPosition * Time.deltaTime);
+            if (door.transform.position.y >= openPosition.y)
+            {
+                door.transform.position = openPosition;
+            }
+        }
+        else if(open == false)
+        {
+            door.transform.Translate(closing * closeSpeed * Time.deltaTime);
+            if (door.transform.position.y <= closedPosition.y)
+            {
+                door.transform.position = closedPosition;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        open = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        open = false;
+    }
+}
